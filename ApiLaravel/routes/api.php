ab6120dfe176f374874d49bc7fa122c5ab70e167
1clogon1 <94410404+1clogon1@users.noreply.github.com>
2024-04-11 00:36:51 +0300

    Update api.php

--- ApiLaravel/routes/api.php
+++ ApiLaravel/routes/api.php
@@ -24,16 +24,24 @@ Route::post('/worker/register', [WorkerController::class,'register']);
 Route::middleware(['auth:worker_token'])->group(function(){
 //Вывод
 Route::get('/auto', [AutoController::class,'index']);
+
 Route::get('/driver', [DriverController::class,'index']);
-Route::get('/rent_car', [RentCArController::class,'index']);
+
+Route::get('/rent_car', [RentCarController::class,'index']);
+    Route::get('/rent_car/category', [RentCarController::class,'index_category']);
+    Route::get('/rent_car/model', [RentCarController::class,'index_model']);
+
 Route::get('/worker', [WorkerController::class,'index']);
 
 
 
 //Отправка
 Route::post('/auto/create', [AutoController::class,'create']);
+
 Route::post('/driver/create', [DriverController::class,'create']);
+
 Route::post('/rent_car/create', [RentCArController::class,'create']);
+
 Route::post('/worker/create', [WorkerController::class,'create']);
 });
 
